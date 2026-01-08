52528eec73628fa8e68421f059f71af7929c626e
Pranjal Kothari <pranjalk2902@gmail.com>
2026-01-08 10:31:55 +0530

    current_plus_one_verse v1.01: small change made in the verse selection process wherein, a verse is continuously randomly selected from the selectableVerses if 1) the next verse is not in selectable universe or 2) if both next verse and previous verse are not in selectable universe
    This is done to ensure that tester does not have to, as far as possible, repeat the next verse if it was already tested in previous rounds

--- current_plus_one_verse/script.js
+++ current_plus_one_verse/script.js
@@ -1649,6 +1649,15 @@ function getNextVerse(chapter, verse) {
     return null; 
 }
 
+function getPreviousVerse(chapter, verse) {
+    if (verse > 1) {
+        return { chapter, verse: verse - 1 };
+    } else if (chapter > 1) {
+        return { chapter: chapter - 1, verse: CHAPTER_VERSES[chapter - 1] };
+    }
+    return null; 
+}
+
 function replenishGlobalUniverse() {
     let chaptersReplenished = [];
 
@@ -2049,10 +2058,30 @@ function handleGenerateVerse() {
          return;
     }
 
-    const randomIndex = Math.floor(Math.random() * selectableVerses.length);
-    const selectedVerse = selectableVerses[randomIndex];
-    const { chapter, verse } = parseVerse(selectedVerse);
+    // Selecting verse at random
+    // Looping until either: 1) The next verse is in the selectableVerses or 2) the next verse and previous verse both are not in the selectableVerses
+    while (true) {
+        
+        var randomIndex = Math.floor(Math.random() * selectableVerses.length);    
+        var selectedVerse = selectableVerses[randomIndex];
+        var { chapter, verse } = parseVerse(selectedVerse);
+        var nextVerseObj = getNextVerse(chapter, verse);
+        var previousVerseObj = getPreviousVerse(chapter, verse);
 
+        var nextVerseStr = nextVerseObj ? formatVerse(nextVerseObj.chapter, nextVerseObj.verse) : null;
+        var previousVerseStr = previousVerseObj ? formatVerse(previousVerseObj.chapter, previousVerseObj.verse) : null;
+
+        if (globalUniverse.includes(nextVerseStr)) {
+            break; // Accept this verse and exit the loop as the next verse is in selectableVerses
+        } else if (!globalUniverse.includes(previousVerseStr) && !globalUniverse.includes(nextVerseStr)) {
+            break; // Accept this verse and exit the loop as both previous and next verses are not in selectableVerses and this verse has to be chosen otherwise this verse will never get chosen
+        } else { 
+            // console.log("Re-rolling verse selection as next verse is not in selectableVerses"); 
+            // console.log(`Selected Verse: ${selectedVerse}, Next Verse: ${nextVerseStr}, Previous Verse: ${previousVerseStr}`);
+            // console.log(`Selectable Verses were: ${globalUniverse.join(', ')}`);
+        }
+    }    
+    
     currentGeneratedKey = selectedVerse;
     renderMainDisplay(); 
 
